868c607385bb7494c17dbcccd7305f3e5850a9b2
seidillaelnur <seidillaelnur@gmail.com>
2019-02-05 17:02:20 +0600

    LAB1

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());  // присваивание значение к int
+
+            for (int i = 1; i <= n; i++)  //  цикл 1
+            {
+                for (int j = 1; j <= i; j++)  //  цикл 2
+                {
+                    Console.Write("[*]"); // вывод
+                }
+                Console.WriteLine();  // переход на следующюю строку
+            }
+            Console.ReadKey();
+        }
+    }
+}
