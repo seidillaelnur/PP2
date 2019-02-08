0cabae37891c63fa5acd4c5e89e3bfdec8374e0d
seidillaelnur <seidillaelnur@gmail.com>
2019-02-08 15:42:05 +0600

    lab2

---
+++ Lab2/Task1/Task1/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = Console.ReadLine();
+
+            int n = s.Length;
+            bool a = true;
+            for (int i = 0; i < n / 2; i++) // цикл 1
+            {
+                if (s[i] != s[n - i - 1]) a = false; // сравниваю 0 и n-1, 1 и n-2 и.т.д
+            }
+
+            if ( a == true ) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+            Console.ReadKey();
+        }
+    }
+}
