88e5c177747be1ee8b00d89b646d192515faf0df
seidillaelnur <seidillaelnur@gmail.com>
2019-02-13 15:49:52 +0600

    lab 2

---
+++ Lab3/Task1/Task1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFarManager
+{
+    class Program
+    {
+
+        static void showState(DirectoryInfo cur, int pos)
+        {
+            FileSystemInfo[] infos = cur.GetFileSystemInfos();
+
+            for (int i = 0; i < infos.Length; i++)
+            {
+                
+                if (i == pos)
+                    Console.BackgroundColor = ConsoleColor.White;
+                else
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    
+                //Console.BackgroundColor = i == pos ? ConsoleColor.White : ConsoleColor.Black;
+
+                
+                if(infos[i].GetType() == typeof(DirectoryInfo))
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                else
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    
+                //Console.ForegroundColor = infos[i].GetType() == typeof(DirectoryInfo) ? ConsoleColor.Magenta : ConsoleColor.Green;
+
+                Console.WriteLine(infos[i].Name);
+            }
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int pos = 0;
+
+            DirectoryInfo dir = new DirectoryInfo(@"D:\PP2");
+
+            while (true)
+            {
+                Console.Clear();
+                showState(dir, pos);
+
+                ConsoleKeyInfo btn = Console.ReadKey();
+                switch (btn.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        pos--;
+                        if (pos < 0)
+                            pos = dir.GetFileSystemInfos().Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        pos++;
+                        if (pos == dir.GetFileSystemInfos().Length)
+                            pos = 0;
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo f = dir.GetFileSystemInfos()[pos];
+                        if (f.GetType() == typeof(DirectoryInfo))
+                        {
+                            dir = new DirectoryInfo(f.FullName);
+                            pos = 0;
+                        }
+                        else
+                        {
+                            Process.Start(f.FullName);
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        dir = dir.Parent;
+                        break;
+                }
+            }
+        }
+    }
+}
